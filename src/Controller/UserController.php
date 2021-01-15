ed9d5ffe01ddb1ead7942a6024f88188ba15fb1c
zubair-haider <zubair.haider@gigalabs.co>
2021-01-15 14:06:36 +0500

    adding api's to update user profile

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -7,6 +7,59 @@ use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class UserController extends ApiController
 {
+    public function userProfile(HttpClientInterface $client,Request $request)
+    {
+        try {
+            $token = $request->headers->get('Authorization');
+            $url = $_ENV['AUTH_URL'] . "api/user/me/";
+            $response = $client->request('GET', $url,[
+                'headers' => [
+                    'Authorization' => $token,
+                ],
+            ]);
+            $data = $response->toArray()['success'];
+            return $this->respondWithSuccess($data);
+        } catch (\Exception $e) {
+            return $this->respondValidationError($e->getMessage());
+        }
+    }
+
+    public function userProfileUpdate(Request $request, HttpClientInterface $client)
+    {
+        try {
+            $token = $request->headers->get('Authorization');
+            $request = $this->transformJsonBody($request);
+            $roles = $request->get('roles');
+            $url = $_ENV['AUTH_URL'] . "api/user/me";
+            $response = $client->request('PUT', $url,
+                [
+                    'headers' => [
+                        'Authorization' => $token,
+                    ],
+                    'json' => ['roles' => $roles],
+                ]);
+            return $this->respondWithSuccess("User updated");
+        } catch (\Exception $e) {
+            return $this->respondValidationError($e->getMessage());
+        }
+    }
+
+    public function userProfileDelete(HttpClientInterface $client)
+    {
+        try {
+            $token = $request->headers->get('Authorization');
+            $url = $_ENV['AUTH_URL'] . "api/user/me/";
+            $response = $client->request('DELETE', $url,[
+                'headers' => [
+                    'Authorization' => $token,
+                ],
+            ]);
+            return $this->respondWithSuccess("user deleted");
+        } catch (\Exception $e) {
+            return $this->respondValidationError($e->getMessage());
+        }
+    }
+
     public function showUser($id, HttpClientInterface $client, Request $request)
     {
         try {
